e525ce3813e04ea6a06dced6add41aee25acf36c
WilliamsAdm <i2310607@continental.edu.pe>
2024-10-11 12:36:21 -0500

    paginacion

--- src/app/modules/employees/pages/employees-list/employees-list.component.ts
+++ src/app/modules/employees/pages/employees-list/employees-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Datum, gEmployees, Links, Meta } from '../../../../shared/models/employee';
 import { EmployeeService } from '../../../../shared/services/employee.service';
-
+import { environment } from '../../../../../environments/environment.development';
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
@@ -11,10 +11,13 @@ export class EmployeesListComponent implements OnInit{
   employeesList: Datum[] = [];
   links: Links | undefined;
   meta: Meta | undefined;
+  env = environment;
+  totalRecords: number = 0;
+  loading: boolean = true;
 
   constructor(private employeeService: EmployeeService) { }
   
-  ngOnInit(): void {
+  /* ngOnInit(): void {
     this.employeeService.getAllEmployees().subscribe((data: gEmployees) => {
       this.employeesList = data.data;
       this.links = data.links;
@@ -22,6 +25,23 @@ export class EmployeesListComponent implements OnInit{
 
       console.log(this.employeesList, this.links, this.meta);
     })
+  } */
+
+  ngOnInit(): void {
+    this.loadEmployees({ first: 0, rows: 15 });  // Cargar la primera página por defecto
   }
 
+  loadEmployees(event: any) {
+    this.loading = true;
+
+    const page = event.first / event.rows + 1;  // Calcular la página actual (página es 1-indexada)
+    const perPage = event.rows;  // Número de registros por página
+
+    this.employeeService.getEmployees(page, perPage).subscribe((data: gEmployees) => {
+      this.employeesList = data.data;
+      this.totalRecords = data.meta.total;  // Asigna el número total de registros
+      this.loading = false;
+    });
+  }
+  
 }
--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
@@ -11,8 +11,19 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  public getAllEmployees(): Observable<gEmployees>{
+  /* public getAllEmployees(): Observable<gEmployees>{
     return this.http.get<gEmployees>(
       `${environment.backendBaseUrl}/api/v1/employees`);
+  } */
+  
+  public getEmployees(page: number, perPage: number): Observable<gEmployees> {
+    // Añadimos los parámetros de paginación a la solicitud
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('per_page', perPage.toString());
+
+    return this.http.get<gEmployees>(
+      `${environment.backendBaseUrl}/api/v1/employees`, { params });
   }
+
 }
