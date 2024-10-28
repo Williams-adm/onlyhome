5a23d17a3ab71db6d9cf92e53c673968634bdbcd
WilliamsAdm <i2310607@continental.edu.pe>
2024-10-28 00:06:42 -0500

    agregando ruta para categories

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesListComponent } from './pages/categories-list/categories-list.component';
+import { CategoriesRoutingModule } from './categories-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { CategoriesListComponent } from './pages/categories-list/categories-list
     CategoriesListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CategoriesRoutingModule
   ]
 })
 export class CategoriesModule { }
