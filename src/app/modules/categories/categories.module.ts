7fac82d6898bab4ecebb298f7827c60e61a8047c
Angel Gabriel <i2311846@continental.edu.pe>
2024-10-27 23:43:30 -0500

    Creando componente Categorias

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoriesListComponent } from './pages/categories-list/categories-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoriesListComponent
+  ],
   imports: [
     CommonModule
   ]
